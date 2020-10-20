46e13ac7267a311067589b4bc62af6635df2c5df
Pace Wasden <pacewasden@gmail.com>
2020-10-19 20:49:20 -0600

    was missing s on templates :|

--- mysite/hobbies/urls.py
+++ mysite/hobbies/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 app_name='hobbies'
 urlpatterns = [
-    path('',views.index, name="index")
+    path('', views.index, name="index"),
+    path('<int: hobbie_id>',views.detail, name="detail"),
 ]
--- mysite/hobbies/views.py
+++ mysite/hobbies/views.py
@@ -7,9 +7,10 @@ from django.template import loader
 
 # Create your views here.
 def index(request):
-    hobby_list= Hobby.objects.all()
-    context={
-        'hobby_list':hobby_list,
+    hobby_list = Hobby.objects.all()
+    template = loader.get_template('hobbies/index.html')
+    context = {
+        'hobby_list': hobby_list,
     }
     return render(request,'hobbies/index.html', context)
-
+    #return HttpResponse(template.render(context, request))
