1b62e4796bf6a1afee192ed56cbf6c339f3d05f6
Pace Wasden <pacewasden@gmail.com>
2020-10-22 19:57:19 -0600

    namespacing figured out on dynamic url

--- mysite/hobbies/urls.py
+++ mysite/hobbies/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 app_name='hobbies'
 urlpatterns = [
     path('', views.index, name="index"),
-    #path('<int: hobbie_id>',views.detail, name="detail"),
+    path('<int:hobby_id>', views.detail, name='detail'),
+    path('item/', views.item, name="item")
 ]
--- mysite/hobbies/views.py
+++ mysite/hobbies/views.py
@@ -8,9 +8,22 @@ from django.template import loader
 # Create your views here.
 def index(request):
     hobby_list = Hobby.objects.all()
-    template = loader.get_template('hobbies/index.html')
+    #    template = loader.get_template('hobbies/index.html')
     context = {
         'hobby_list': hobby_list,
     }
-    return render(request,'hobbies/index.html', context)
-    #return HttpResponse(template.render(context, request))
+    return render(request, 'hobbies/index.html', context)
+    # return HttpResponse(template.render(context, request))
+
+
+def item(request):
+    return HttpResponse('<h1>This is an item view</h1>')
+
+
+def detail(request, hobby_id):
+    # hobby = Hobby.objects.get(pk=hobby_id)
+    # context = {
+    #     'hobby': hobby
+    # }
+    # return render(request, 'hobbies/detail.html', context)
+    return HttpResponse('This is id: %s' % hobby_id)
