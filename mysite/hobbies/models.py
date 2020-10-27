74b5fad819d8d5dc9a997b557c99e6084ccb134b
Pace Wasden <pacewasden@gmail.com>
2020-10-26 19:13:31 -0600

    Changing db

--- mysite/hobbies/models.py
+++ mysite/hobbies/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+
 
 # Create your models here.
 class Hobby(models.Model):
     def __str__(self):
         return self.hobby_name
 
+    user_name = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     hobby_name = models.CharField(max_length=200)
     hobby_description = models.CharField(max_length=200)
-    hobby_picture=models.CharField(max_length=500, default="https://icebreakerideas.com/wp-content/uploads/2018/07/57-Best-List-of-Hobbies-For-Men-and-Women.jpeg")
+    hobby_picture = models.CharField(max_length=500, default="https://icebreakerideas.com/wp-content/uploads/2018/07/57-Best-List-of-Hobbies-For-Men-and-Women.jpeg")
+
+    def get_absolute_url(self):
+        return reverse("hobbies:detail", kwargs={"pk": self.pk})
